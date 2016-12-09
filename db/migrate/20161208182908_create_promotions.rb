83061f716ec01aa9beb43989efb98ed1db1d90e9
Luiz Carvalho <maximusmano@gmail.com>
2016-12-09 11:27:36 -0300

    update database

--- db/migrate/20161208182908_create_promotions.rb
+++ db/migrate/20161208182908_create_promotions.rb
@@ -3,6 +3,10 @@ class CreatePromotions < ActiveRecord::Migration[5.0]
     create_table :promotions do |t|
       t.string :title
       t.string :url
+      t.string :value
+      t.string :discount
+      t.string :thumb
+      t.string :image
       t.string :origin
       t.string :identifier
       t.string :promoter
